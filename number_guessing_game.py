89134333ba8b221ed768afff494f3ad8a525a71c
harshitaaa0 <harshitakushwaha48@gmail.com>
2025-07-07 23:01:55 +0530

    Update guessing.txt

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,6 +2,7 @@
 def number_guessing_game_created():
     print("Welcome To The Number Guessing Game!")
     print("Guess a number between 1 & 100")
+
     # Function to generate a unique number
     def generate_unique_number():
         now = str(time.time())  # get current time in seconds
@@ -13,15 +14,18 @@ def number_guessing_game_created():
     # storing the generated number in a variable
     secret_number = generate_unique_number()
     print("Generated:", secret_number)
+
     # initializing the number of attempts
     attempts = 0
+
     while attempts < 5:
         try:
             # Taking input from the user
             guess = int(input("Your Guess: "))
         except ValueError:
             print("Please enter a valid integer!")
-            continue 
+            continue
+
         if guess < secret_number:
             print("Too Low!")
         elif guess > secret_number:
@@ -29,17 +33,19 @@ def number_guessing_game_created():
         else:
             print("The secret number is correct", guess)
             break
+
         # Incrementing the number of attempts
         attempts += 1
+
     if attempts == 5:
         print("You have used all your attempts. The secret number was:", secret_number)
 
-        # Offer a rematch
-        rematch = input("Do you want to play again? (yes/no): ")
-        if rematch.lower() == "yes":
-            number_guessing_game_created()
-        else:
-            print("Thank you for playing!")
+    # Offer a rematch
+    rematch = input("Do you want to play again? (yes/no): ")
+    if rematch.lower() == "yes":
+        number_guessing_game_created()
+    else:
+        print("Thank you for playing!")
 
 # Call the function to start the game
 number_guessing_game_created()
